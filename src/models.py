0496c9371ae29ad7bf5e9099deedebadaaa38899
Genesis Longart <102156923+GenyLongart@users.noreply.github.com>
2023-08-22 20:40:23 +0000

    Agregando modelos

--- src/models.py
+++ src/models.py
@@ -7,38 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Users(Base):
+class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String(100), nullable=False)
     firstname = Column(String(100), nullable=False)
     lastname = Column(String(100), nullable=True)
     email = Column(String(100), nullable=False)
+    posts = relationship('Post', backref="user")
+    comments = relationship('Comment', backref="user")
 
-class Posts(Base):
+class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    media = relationship('Media', backref="post")
 
-class Comments(Base):
+class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(150), nullable=False)
-    author_id = Column(Integer, ForeignKey=True)
-    post_id = Column(Integer, ForeignKey=True)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
 
-class Medias(Base):
+class Media(Base):
     __tablename__ = 'medias'
     id = Column(Integer, primary_key=True)
-    type = Column(Enum, nullable=False)
+    type = Column(String, nullable=False)
     url = Column(String, nullable=False)
-    post_id = Column(Integer, ForeignKey=True)
+    post_id = Column(Integer, ForeignKey('posts.id'))
 
-class Followers(Base):
+class Follower(Base):
     __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey=True)
-    user_to_id = Column(Integer, ForeignKey=True)
+    user_from_id = Column(Integer)
+    user_to_id = Column(Integer)
 
 ## Draw from SQLAlchemy base
 try:
